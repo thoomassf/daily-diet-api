27a84e211d6b2dcee6b43745c48950a58836f625
Thomas Silva de Farias <thomasfarias19@gmail.com>
2025-06-24 20:30:11 -0300

    Model daily and user

--- models/daily.py
+++ models/daily.py
@@ -2,8 +2,19 @@ from database import db
 
 
 class Daily(db.Model):
-    id: db.Column(db.Integer, primary_key=True)
-    name: db.Column(db.String(100), nullable=False)
-    description: db.Column(db.String(200))
-    data_time: db.Column(db.DateTime, nullable=False)
-    is_diet: db.Column(db.Boolean, default=False)
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    description = db.Column(db.String(200))
+    data_time = db.Column(db.DateTime, nullable=False)
+    is_diet = db.Column(db.Boolean, default=False)
+    user = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
+
+    def to_disc(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "description": self.description,
+            "data_time": self.data_time.strftime("%d/%m/%Y %H:%M"),
+            "is_diet": self.is_diet,
+            "user": self.user,
+        }
---
+++ models/user.py
@@ -0,0 +1,9 @@
+from flask_login import UserMixin
+
+from database import db
+
+
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
